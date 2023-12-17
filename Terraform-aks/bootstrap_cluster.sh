8f228b5b1a5220063cca8cc321fbc04dac2447d3
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2023-12-17 20:35:41 +0530

    Create bootstrap_cluster.sh

---
+++ Terraform-aks/bootstrap_cluster.sh
@@ -0,0 +1,13 @@
+#!/bin/sh  
+
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+kubeadm init   >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/calico.yaml -O >/dev/null 2>&1
