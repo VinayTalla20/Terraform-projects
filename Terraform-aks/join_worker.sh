1a47c23b1e2e4385bcb7f05dda88724bc7857402
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2024-01-01 12:43:14 +0530

    Update join_worker.sh

--- Terraform-aks/join_worker.sh
+++ Terraform-aks/join_worker.sh
@@ -1,6 +1,6 @@
-# get kubeadm join commad from master
+#!/bin/sh
 
-sudo su
+# get kubeadm join commad from master
 apt install -qq sshpass -y
 sshpass -p "terraformuser" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no terraformuser@$MASTER_IPADDRESS:home/terraformuser/joincluster.sh /joincluster.sh 2>/dev/null
 chmod +x joincluster.sh
