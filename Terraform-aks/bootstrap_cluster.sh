4bc85b0daf1098d409fca2ff5dde362b65b61053
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2024-01-01 13:12:38 +0530

    Update bootstrap_cluster.sh

--- Terraform-aks/bootstrap_cluster.sh
+++ Terraform-aks/bootstrap_cluster.sh
@@ -4,14 +4,13 @@ echo "[TASK 0] Set SSH user and password"
 # add new user and set password
 sudo su
 sudo useradd -m terraformuser
-echo -e "terraformuser\nterraformuser" | sudo passwd terraformuser
+echo "terraformuser\nterraformuser" | passwd terraformuser >/dev/null 2>&1
 sudo usermod --shell /bin/bash terraformuser
 
 
 # Enable password authencation using ssh
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
-systemctl restart ssh
 sleep 5
 
 
@@ -26,5 +25,6 @@ kubeadm init   >> /root/kubeinit.log 2>/dev/null
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
 kubeadm token create --print-join-command > joincluster.sh 2>/dev/null
 cp joincluster.sh  /home/terraformuser/
+chown terraformuser:root /home/terraformuser/joincluster.sh
 
 kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/calico.yaml
