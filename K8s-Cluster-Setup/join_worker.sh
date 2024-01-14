276d4b9c2377400f4b050976b4a9069ea9a24926
Vinay Talla <98005486+VinayTalla20@users.noreply.github.com>
2024-01-14 11:29:32 +0530

    Update join_worker.sh

--- K8s-Cluster-Setup/join_worker.sh
+++ K8s-Cluster-Setup/join_worker.sh
@@ -3,7 +3,8 @@
 # get kubeadm join commad from master
 sudo su
 apt install -qq sshpass -y
-sleep 30
-sshpass -p "terraformuser" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no terraformuser@$MASTER_IPADDRESS:/home/terraformuser/joincluster.sh . 2>/dev/null
+sleep 10
+echo "ssh to master with IPAddress ${MASTER_IPADDRESS}"
+sshpass -p "terraformuser" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no terraformuser@$MASTER_IPADDRESS:/home/terraformuser/joincluster.sh .
 chmod +x joincluster.sh
 sh joincluster.sh
